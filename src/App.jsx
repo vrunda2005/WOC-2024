e0bfa44a02fa4e0b0d35b36729eafa78bdde50a7
kash2104 <kparikh1104@gmail.com>
2024-12-21 23:24:09 +0530

    redirect to past events

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import PastEvents from './pages/PastEvents';
 
 function App() {
   return (
     <>
       <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/past-events' element={<PastEvents/>}/>
+      </Routes>
     </>
   )
 }
