deb10d1bde380bcbfc42dd7eaed50167bbf10054
Vrunda Patel <vrunda.p@ahduni.edu.in>
2024-12-23 10:22:13 +0530

    informationbox_styles_file

--- src/components/InformationBoxComponent/InformationBoxComponent.jsx
+++ src/components/InformationBoxComponent/InformationBoxComponent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./InformationBoxComponentStyles";
 
 const InformationBox = ({ title, imagePath, description }) => {
   return (
@@ -11,46 +12,6 @@ const InformationBox = ({ title, imagePath, description }) => {
       <p style={styles.description}>{description}</p>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    background: "linear-gradient(135deg, rgb(34, 172, 200), rgb(5, 68, 85))",
-    padding: "20px",
-    borderRadius: "12px",
-    textAlign: "center",
-    color: "white",
-    width: "320px",
-    margin: "20px auto",
-    position: "relative",
-  },
-  iconContainer: {
-    width: "70px",
-    height: "70px",
-    borderRadius: "50%",
-    background: "#004457",
-    position: "absolute",
-    top: "-35px",
-    left: "50%",
-    transform: "translateX(-50%)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  icon: {
-    width: "100%",
-    height: "100%",
-    borderRadius: "50%",
-    objectFit: "cover",
-  },
-  title: {
-    fontSize: "20px",
-    margin: "40px 0 10px",
-  },
-  description: {
-    fontSize: "14px",
-    lineHeight: "1.4",
-  },
 };
 
 InformationBox.propTypes = {
